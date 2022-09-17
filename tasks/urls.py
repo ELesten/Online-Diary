17eadc36c7560746d504219bfc5bf586a3bbae90
Дмитрий Чадаев <chadaevd@mail.ru>
2022-09-17 23:14:23 +0300

    refactor: Added endpoint TaskComment. Other endpoints have been updated.

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register(r"tasks", TaskModelViewSet)
 router.register(r"homework", HomeworkModelViewSet)
+router.register(r"comment", TaskCommentModelViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("my-homework/", StudentHomeworkListCreateApiView.as_view()),
--- tasks/views.py
+++ tasks/views.py
@@ -108,6 +108,60 @@ class StudentHomeworkDetailApiView(APIView):
 
 
 class TaskCommentModelViewSet(ModelViewSet):
-    queryset = TaskComment
+    queryset = TaskComment.objects.all()
     serializer_class = TaskCommentSerializer
     permission_classes = [IsAuthenticated]
+
+    def role_check(self, request):
+        if request.user.role == "Student":
+            queryset = self.filter_queryset(
+                self.get_queryset().filter(
+                    task__responsible_group=request.user.group
+                )
+            )
+        elif request.user.role == "Teacher":
+            queryset = self.filter_queryset(
+                self.get_queryset().filter(
+                    task__responsible_group__in=request.user.lead_groups.values("id")
+                )
+            )
+        else:
+            queryset = self.filter_queryset(self.get_queryset())
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        user_group = Task.objects.filter(responsible_group=request.user.group).values_list("id", flat=True)
+        if int(request.data.get("task")) not in user_group:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        return super().create(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.role_check(request)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.author.username != request.user.username and request.user.role != "Manager":
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        if instance.author.username != request.user.username and request.user.role != "Manager":
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        self.perform_update(serializer)
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
