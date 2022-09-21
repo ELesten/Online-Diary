e1c22922b50213723a73d17b56256ffc34fb5539
Дмитрий Чадаев <chadaevd@mail.ru>
2022-09-21 07:45:00 +0300

    feat: Added endpoint for working with groups and related dependencies

--- apps/groups/views.py
+++ apps/groups/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from project_settings.permissions import *
+from .serializers import *
 
-# Create your views here.
+
+class GroupModelViewSet(ModelViewSet):
+    """
+    CRUD to work with groups by the managers/teachers.
+    """
+    serializer_class = GroupSerializer
+    permission_classes = [IsSchoolRepresentative]
+    queryset = Group.objects.all()
+
+
