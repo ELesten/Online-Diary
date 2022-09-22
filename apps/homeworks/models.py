4eeee1111d1b27607189d9eb9ada8ab7cef4d737
Дмитрий Чадаев <chadaevd@mail.ru>
2022-09-22 21:00:23 +0300

    feat: Added a signal to credit currency to the user.

--- apps/homeworks/models.py
+++ apps/homeworks/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from rest_framework.generics import get_object_or_404
+from django.db.models import F
 from apps.users.models import CustomUser
 from apps.tasks.models import Task
+from django.db.models import signals
+from django.dispatch import receiver
 
 # CHOICES
 
@@ -23,6 +27,7 @@ class Homework(models.Model):
         default=HomeworkStatus.PENDING_VERIFICATION
     )
     author = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, related_name="homework")
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return f"Homework for the task {self.connection_with_task.task_name} by {self.author.username}"
