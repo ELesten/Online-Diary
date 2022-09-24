2aa3dc60474f39013be892c6eb3432617c49ae46
Дмитрий Чадаев <chadaevd@mail.ru>
2022-09-24 22:00:05 +0300

    refactor: Added endpoint to change user's password. Removed 'djoser' package.

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -5,4 +5,18 @@ from .models import *
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = "__all__"
+        fields = ['email', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = CustomUser(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        instance.password = validated_data.get('password', instance.password)
+        instance.set_password(instance.password)
+        instance.save()
+        return instance
